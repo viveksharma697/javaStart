c7cedc758f20b0e18750e19f13b905b6c9c31c10
viveksharma697 <viveksharma697@gmail.com>
2022-05-07 23:35:01 +0530

    Starting the basics of the Java

---
+++ Inputs.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+        
+    }
+}
